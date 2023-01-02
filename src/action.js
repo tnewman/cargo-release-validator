2c0bf3b694aa18146f53bd2b73e4a7b0ee7a1e84
Thomas Newman <tnewman@users.noreply.github.com>
2023-01-01 23:12:54 -0500

    Log validation

--- src/action.js
+++ src/action.js
@@ -1,12 +1,13 @@
-import {setFailed} from "@actions/core";
+import {info, setFailed} from "@actions/core";
 import {context} from "@actions/github";
 import validate from "./validate.js";
 
 export default async function action() {
     try {
         const ref = context.ref;
-        //const prefix = core.getInput('prefix');
-        //validate(ref, prefix);
+        const prefix = core.getInput('prefix');
+
+        validate(ref, prefix);
     } catch (error) {
         setFailed(error.message);
     }
