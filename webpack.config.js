45d228646c204efcf337334a4611a2a43fffe782
Thomas Newman <tnewman@users.noreply.github.com>
2023-01-01 22:51:03 -0500

    Output ES module

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const config = {
     target: "node16",
     mode: "production",
     output: {
-        filename: 'index.js'
+        filename: 'index.js',
+        module: true
+    },
+    experiments: {
+        outputModule: true
     }
 };
 
