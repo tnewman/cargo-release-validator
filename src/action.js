d92399e15909e5ce26f293ede1473db72c995dd5
Thomas Newman <tnewman@users.noreply.github.com>
2023-01-01 23:14:36 -0500

    Fixed imports

--- src/action.js
+++ src/action.js
@@ -1,11 +1,11 @@
-import {info, setFailed} from "@actions/core";
+import {getInput, setFailed} from "@actions/core";
 import {context} from "@actions/github";
 import validate from "./validate.js";
 
 export default async function action() {
     try {
         const ref = context.ref;
-        const prefix = core.getInput('prefix');
+        const prefix = getInput('prefix');
 
         validate(ref, prefix);
     } catch (error) {
