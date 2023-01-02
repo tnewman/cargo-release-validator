30add8f9799f09c82229d6669090026b44df07b3
Thomas Newman <tnewman@users.noreply.github.com>
2023-01-01 23:06:40 -0500

    Update imports

--- src/action.js
+++ src/action.js
@@ -1,13 +1,13 @@
-import core from "@actions/core";
-import github from "@actions/github";
+import {setFailed} from "@actions/core";
+import {context} from "@actions/github";
 import validate from "./validate.js";
 
 export default async function action() {
     try {
-        const ref = github.context.ref;
+        const ref = context.ref;
         //const prefix = core.getInput('prefix');
         //validate(ref, prefix);
     } catch (error) {
-        core.setFailed(error.message);
+        setFailed(error.message);
     }
 }
