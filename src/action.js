311d79fde53a43db7394d3a6b3e494767daf39cb
Thomas Newman <tnewman@users.noreply.github.com>
2023-01-01 22:55:48 -0500

    Log context

--- src/action.js
+++ src/action.js
@@ -6,10 +6,10 @@ export default async function action() {
     try {
         console.log(JSON.stringify(github.context));
 
-        const ref = github.context.ref;
-        const prefix = core.getInput('prefix');
-        validate(ref, prefix);
+        //const ref = github.context.ref;
+        //const prefix = core.getInput('prefix');
+        //validate(ref, prefix);
     } catch (error) {
-        core.setFailed(error.message);
+        //core.setFailed(error.message);
     }
 }
